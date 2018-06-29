0ee7eb4125ca51ba29ee03ab1b9a81ba22dc0c07
Railson Tales <railson777@gmail.com>
2018-06-29 02:49:09 -0300

    Add files via upload

---
+++ mybot.js
@@ -0,0 +1,44 @@
+var TelegramBot = require( "node-telegram-bot-api" );
+var bot = new TelegramBot( "556064998:AAG-mxfV97VDHjdmxhY0QgHyj-9jMEo2fCo", { polling: true } );
+bot.onText( /\/start/, function( msg ) {
+  bot.sendMessage(
+      msg.from.id,
+      "Olá <b>" + msg.from.first_name + "</> " + msg.from.last_name + "\nVamos jogar um jogo?\nhttps://esquilovoadorbot.000webhostapp.com/",
+      {
+          parse_mode: "HTML"
+     }
+  );
+} );
+bot.onText( /\/play (.+)/, function( msg, match ) {
+  var fromId = msg.from.id;
+  switch( match[1] ) {
+      case "TestGame":
+          bot.sendGame(
+              fromId,
+              "TestGame",
+              {
+                  reply_markup: JSON.stringify({
+                      inline_keyboard: [
+                          [ { text: "Play", callback_game: JSON.stringify( { game_short_name: "TestGame" } ) } ],
+                          [ { text: "Share", url: "https://t.me/Esquilo_Voador_bot?game=EsquiloVoador" } ]
+                      ]
+                  })
+              }
+          );
+          break;
+      default:
+          bot.sendMessage( fromId, "Digite somente o comando /start para começar a jogar!" );
+}});
+bot.on( "callback_query", function( cq ) {
+  if ( cq.game_short_name ) {
+      switch( cq.game_short_name ) {
+          case "TestGame":
+              bot.answerCallbackQuery( cq.id, undefined, false, { url: "https://esquilovoadorbot.000webhostapp.com/" } );
+              return;
+      }
+      bot.answerCallbackQuery( cq.id, "Digite o comando /start para começar a jogar!", true );
+  }
+} );
+bot.on( "inline_query", function( iq ) {
+  bot.answerInlineQuery( iq.id, [ { type: "game", id: "0", game_short_name: "TestGame" } ] );
+} );
